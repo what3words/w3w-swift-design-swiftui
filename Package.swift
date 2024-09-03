de81339917259c2fe972bf319ec0c9f9fe982aa3
Dave Duprey <dave@what3words.com>
2024-09-03 12:49:41 +0100

    reset dependancy version

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     platforms: [.iOS("13.0")],
     products: [.library(name: "W3WSwiftDesignSwiftUI", targets: ["W3WSwiftDesignSwiftUI"])],
 
-    dependencies: [.package(url: "https://github.com/what3words/w3w-swift-themes.git", branch: "staging")],
+    dependencies: [.package(url: "https://github.com/what3words/w3w-swift-themes.git", "1.0.0" ..< "2.0.0")],
 
     targets: [
       .target(name: "W3WSwiftDesignSwiftUI", dependencies: [.product(name: "W3WSwiftThemes", package: "w3w-swift-themes")]),
