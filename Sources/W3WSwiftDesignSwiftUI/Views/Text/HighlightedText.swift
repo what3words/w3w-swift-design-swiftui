0d9037bc8eb4a8da5a79f94c59e3843e0218cb1a
Kaley Nguyen <kaley.nguyen@what3words.com>
2025-07-30 14:38:53 +0700

    Add HighlightedText

---
+++ Sources/W3WSwiftDesignSwiftUI/Views/Text/HighlightedText.swift
@@ -0,0 +1,59 @@
+//
+//  HighlightedText.swift
+//  w3w-swift-design-swiftui
+//
+//  Created by Kaley Nguyen on 30/7/25.
+//
+import SwiftUI
+import W3WSwiftThemes
+
+@available(iOS 15, *)
+public struct HighlightedText: View {
+  let text: String
+  let color: Color?
+  let font: W3WFont?
+  let highlightedText: String?
+  let highlightedTextColor: Color?
+  let highlightedTextFont: W3WFont?
+  
+  public init(text: String,
+              color: Color? = nil,
+              font: W3WFont? = nil,
+              highlightedText: String? = nil,
+              highlightedTextColor: Color? = nil,
+              highlightedTextFont: W3WFont? = nil) {
+    self.text = text
+    self.color = color
+    self.font = font
+    self.highlightedText = highlightedText
+    self.highlightedTextColor = highlightedTextColor
+    self.highlightedTextFont = highlightedTextFont
+  }
+  
+  public var body: some View {
+    Text(makeAttributedString())
+  }
+  
+  private func makeAttributedString() -> AttributedString {
+    var attributed = AttributedString(text)
+    attributed.foregroundColor = color
+    attributed.font = font?.suFont
+    
+    if let highlightedText,
+       let range1 = attributed.range(of: highlightedText) {
+      attributed[range1].foregroundColor = highlightedTextColor
+      attributed[range1].font = highlightedTextFont?.suFont
+    }
+    
+    return attributed
+  }
+}
+
+#Preview {
+  if #available(iOS 15, *) {
+    HighlightedText(text: "Hello World", color: .black, highlightedText: "World", highlightedTextColor: .red)
+  } else {
+    // Fallback on earlier versions
+    Text("Hello World")
+  }
+}
